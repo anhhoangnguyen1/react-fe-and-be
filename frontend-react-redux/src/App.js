55818fb938626bfcfadf4b77b87c73a2a17d7352
Nguyễn Anh Hoàng <anhhoangyh43@gmail.com>
2025-08-16 10:55:44 +0700

    update header

--- frontend-react-redux/src/App.js
+++ frontend-react-redux/src/App.js
@@ -2,8 +2,8 @@ import logo from './logo.svg';
 import './App.scss';
 import { useDispatch, useSelector } from 'react-redux';
 import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
-import MyComponent from './components/MyComponent';
 import React from 'react';
+import HeaderReact from './components/header/HeaderReact';
 
 const App = () => {
   const count = useSelector(state => state.counter.count);
@@ -11,8 +11,7 @@ const App = () => {
 
   return (
     <div className="App">
-      hello world
-      <button className='btn btn-primary'>test</button>
+      <HeaderReact></HeaderReact>
     </div>
   );
 }
