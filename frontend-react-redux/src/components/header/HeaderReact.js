8d7cde53a0b61776453dcf434d13f21e31eb3e3c
Nguyễn Anh Hoàng <anhhoangyh43@gmail.com>
2025-08-16 11:40:07 +0700

    css active for header

--- frontend-react-redux/src/components/header/HeaderReact.js
+++ frontend-react-redux/src/components/header/HeaderReact.js
@@ -2,7 +2,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import { Link, Outlet } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 const HeaderReact = () => {
     return (
@@ -12,9 +12,9 @@ const HeaderReact = () => {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Link to='/' className='nav-link'>Home</Link>
-                        <Link to='/users' className='nav-link'>User</Link>
-                        <Link to='/admin' className='nav-link'>Admin</Link>
+                        <NavLink to='/' className='nav-link'>Home</NavLink>
+                        <NavLink to='/users' className='nav-link'>User</NavLink>
+                        <NavLink to='/admin' className='nav-link'>Admin</NavLink>
                         {/* <Nav.Link href="/">Home</Nav.Link>
                         <Nav.Link href="/users">Users</Nav.Link>
                         <Nav.Link href="/admin">Admin</Nav.Link> */}
