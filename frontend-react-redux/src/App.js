2256e1d7e13218a0e43257be334fdfd55c48668b
Nguyễn Anh Hoàng <anhhoangyh43@gmail.com>
2025-08-16 11:31:46 +0700

    navigate header

--- frontend-react-redux/src/App.js
+++ frontend-react-redux/src/App.js
@@ -4,14 +4,25 @@ import { useDispatch, useSelector } from 'react-redux';
 import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
 import React from 'react';
 import HeaderReact from './components/header/HeaderReact';
+import { Link, Outlet } from 'react-router-dom';
 
 const App = () => {
   const count = useSelector(state => state.counter.count);
   const dispatch = useDispatch();
 
   return (
-    <div className="App">
-      <HeaderReact></HeaderReact>
+    <div className="app-container">
+      <div className='header-container'>
+        <HeaderReact></HeaderReact>
+      </div>
+      <div className='main-container'>
+        <div className='sidenav-container'>
+
+        </div>
+        <div className='app-content'>
+          <Outlet></Outlet>
+        </div>
+      </div>
     </div>
   );
 }
--- frontend-react-redux/src/index.js
+++ frontend-react-redux/src/index.js
@@ -4,13 +4,24 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import store from './redux/store';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import User from './components/user/User';
+import Admin from './components/admin/Admin';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
     {/* <React.StrictMode> */}
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App />} >
+          <Route path='users' element={<User />} />
+          <Route path='admin' element={<Admin />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+
     {/* </React.StrictMode> */}
   </Provider>
 );
